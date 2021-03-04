76c8a6b5a9f33b788894c034bbbdf2de3e6dd713
Jonathan <jonfleck88@gmail.com>
2021-03-04 15:21:32 -0500

    Added tests

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -1,23 +1,34 @@
+// Minimum TypeScript Version: 3.9
+
 import type { Dispatch, SetStateAction } from "react";
 
-type ReadonlyMapIncluded<T> = T extends Map<unknown,unknown> ? Omit<T, "set" | "delete" | "clear">
-: Readonly<T>;
+type ReadonlyMapIncluded<T> = T extends Map<unknown, unknown>
+  ? keyof Map<unknown, unknown> & keyof ReadonlyMap<unknown, unknown>
+  : T extends Set<unknown>
+  ? keyof Set<unknown> & keyof ReadonlySet<unknown>
+  : Readonly<T>;
 
 declare module "react" {
-  
-    /**
-     * Returns a stateful value, and a function to update it.
-     *
-     * @version 16.8.0
-     * @see https://reactjs.org/docs/hooks-reference.html#usestate
-     */
-    function useState<S>(initialState: S | (() => S)): [ReadonlyMapIncluded<S>, Dispatch<SetStateAction<ReadonlyMapIncluded<S>>>];
-    // convenience overload when first argument is omitted
-    /**
-     * Returns a stateful value, and a function to update it.
-     *
-     * @version 16.8.0
-     * @see https://reactjs.org/docs/hooks-reference.html#usestate
-     */
-    function useState<S = undefined>(): [ReadonlyMapIncluded<S> | undefined, Dispatch<SetStateAction<ReadonlyMapIncluded<S> | undefined>>];
+  /**
+   * Returns a stateful value, and a function to update it.
+   *
+   * @version 16.8.0
+   * @see https://reactjs.org/docs/hooks-reference.html#usestate
+   */
+  function useState<S>(
+    initialState: S | (() => S)
+  ): [ReadonlyMapIncluded<S>, Dispatch<SetStateAction<ReadonlyMapIncluded<S>>>];
+  // convenience overload when first argument is omitted
+  /**
+   * Returns a stateful value, and a function to update it.
+   *
+   * @version 16.8.0
+   * @see https://reactjs.org/docs/hooks-reference.html#usestate
+   */
+  function useState<S = undefined>(): [
+    ReadonlyMapIncluded<S> | undefined,
+    Dispatch<SetStateAction<ReadonlyMapIncluded<S> | undefined>>
+  ];
 }
+
+export {};
--- packages/react/tests.tsx
+++ packages/react/tests.tsx
@@ -1,5 +1,25 @@
 import * as React from "react";
 
 export default function TestComponent() {
-  return <div />
+  const [readonlyObject] = React.useState({ hello: "world" });
+
+  // @ts-expect-error hello should be read only
+  readonlyObject.hello = "USA";
+
+  const [readonlyArray] = React.useState(["this", "that"]);
+
+  // @ts-expect-error shouldn't allow mutation
+  readonlyArray.push("nope");
+
+  const [readonlyMap] = React.useState(new Map());
+
+  // @ts-expect-error map should now allow set
+  readonlyMap.set("hello", "there");
+
+  const [readonlySet] = React.useState(new Set(["unique"]));
+
+  // @ts-expect-error no mutation
+  readonlySet.add("new");
+
+  return <div />;
 }
