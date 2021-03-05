777917dc98a6bc6367059b28037de16bffc013c7
Jonathan <jonfleck88@gmail.com>
2021-03-05 12:23:31 -0500

    update readmes

--- packages/array.includes/index.d.ts
+++ packages/array.includes/index.d.ts
@@ -1,10 +1,3 @@
-/**
- * Using includes on a const array is a pain. This patch allows a broader type
- * to be passed to includes and uses a type predicate to narrow the type.
- * A common example is using a const array to represent valid states.
- * Often we'll want to check if a generic string coming from user input
- * is a valid state, this patch allows us to use .includes for that.
- */
 // Minimum TypeScript Version: 3.9
 
 declare global {
---
+++ packages/react/README.md
@@ -0,0 +1,3 @@
+Updates useState to return readonly state to help prevent state mutation bugs.
+
+![editor error when trying to push to readonly array returned from our modified use state](readonly-use-state.png)
