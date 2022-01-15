0fe271b7e64389147b8d2a9c5e09e52fa73b3934
Jonathan <jonfleck88@gmail.com>
2022-01-15 17:26:50 -0500

    import json.parse type

--- packages/all/index.d.ts
+++ packages/all/index.d.ts
@@ -2,3 +2,4 @@ import "@pragmatically-typed/array.includes";
 import "@pragmatically-typed/body.json";
 import "@pragmatically-typed/react";
 import "@pragmatically-typed/eval";
+import "@pragmatically-typed/json.parse";
